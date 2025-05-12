52423261bf476538984db7e948e48fb46b786ab1
noamandorf <noatomasmandorf@smccd.edu>
2025-05-11 19:11:13 -0700

    added responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,8 @@ import {Link} from "react-router-dom";
 import headerData from '../../json/headerData.json';
 
 const Header = () => {
+
+
     return (
         <header className='inset-y-0'>
             <div className='relative w-full'>
@@ -24,19 +26,20 @@ const Header = () => {
                     z-10"
                 />
             </div>
-
-            <div
-                className="flex flex-row justify-between items-center pl-2 pr-5 py-4 mt-10 relative bg-white mx-auto">
-                <div className="flex gap-2 text-lg font-semibold">
-                    {headerData.header.navigation.left.map((link, index) => (
-                        <Link
-                            key={index}
-                            to={link.href}
-                            className="pr-5 hover:opacity-55 cursor-pointer transition-opacity duration-300"
-                        >
-                            {link.label}
-                        </Link>
-                    ))}
+            <div className='w-full overflow-hidden overflow-x-auto'>
+                <div
+                    className="mt-10 relative w-[575px]">
+                    <div className="text-lg font-semibold w-full p-[2%]">
+                        {headerData.header.navigation.left.map((link, index) => (
+                            <Link
+                                key={index}
+                                to={link.href}
+                                className="hover:opacity-55 cursor-pointer transition-opacity duration-300 pr-4"
+                            >
+                                {link.label}
+                            </Link>
+                        ))}
+                    </div>
                 </div>
             </div>
 
